e22303124418457e0c2b05ae73a802429a7f14bf
Kapil Bhalodiya <kapilbhalodiya91041@gmail.com>
2025-04-11 20:19:13 +0530

    Add Radio-Variant Component

---
+++ frontend/src/app/compoments/quantity-box/quantity-box.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quantity-box',
+  imports: [],
+  templateUrl: './quantity-box.component.html',
+  styleUrl: './quantity-box.component.scss'
+})
+export class QuantityBoxComponent {
+
+}
