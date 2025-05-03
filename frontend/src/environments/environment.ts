cc3828d4f29ce9ff9a5661f5fc984ec071e47193
Kapil Bhalodiya <kapilbhalodiya91041@gmail.com>
2025-05-03 22:04:39 +0530

    Update Ingress Service-name dynamic

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,4 +2,5 @@ export const environment = {
     production: false,
     apiUrl: 'http://localhost:8000/api'
 };
-  
+
+export const STRIPE_PK = "pk_test_51REP3lDBxXhwa9k2yzLbnYdr8eS6DuHnP9SbsYouNdlkZPK8GRhVfF1Bh9iHR1Vz5VwWc1b5BUIjKb5tJtPMT6lp00Ke5xIjSX"
