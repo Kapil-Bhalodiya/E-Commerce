a8c4dd0d4284258c537a8d05226e9e8888cb0d82
Kapil Bhalodiya <kapilbhalodiya91041@gmail.com>
2025-06-18 21:12:31 +0530

    Update DropDown Default Option

--- frontend/src/app/compoments/filter/dropdown-filter/dropdown-filter.component.ts
+++ frontend/src/app/compoments/filter/dropdown-filter/dropdown-filter.component.ts
@@ -1,20 +1,43 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges, OnChanges } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
+// Define an interface for the dropdown items to handle both value and id
+interface DropdownItem {
+  label: string;
+  value?: string; // Optional for dropDownList
+  id?: string;    // Optional for dropDownSort, dropPageSort
+}
 
 @Component({
   selector: 'app-dropdown-filter',
+  standalone: true,
   imports: [FormsModule, CommonModule],
   templateUrl: './dropdown-filter.component.html',
-  styleUrl: './dropdown-filter.component.scss'
+  styleUrls: ['./dropdown-filter.component.scss']
 })
-export class DropdownFilterComponent {
-  @Input() items: any[] = [];
-  size: string = 'medium';
-  selectedOption: string = this.items[0];
+export class DropdownFilterComponent implements OnInit, OnChanges {
+  @Input() items: DropdownItem[] = [];
+  @Input() size: string = 'medium';
+  @Input() defaultOption: DropdownItem = { label: '-- Select --', value: '' };
+  selectedOption: string | undefined = ''; 
 
-  setSize(size: string): void {
-    this.size = size;
+  ngOnInit(): void {
+    this.setDefaultOption();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['items'] && this.items.length > 0) {
+      this.setDefaultOption();
+    }
+  }
+
+  setDefaultOption(): void {
+    if (this.items && this.items.length > 0) {
+      this.selectedOption = this.items[0].value;
+    } else {
+      this.selectedOption = '';
+    }
   }
 
   onSortChange(): void {
