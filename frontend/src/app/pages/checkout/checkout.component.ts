9402fae26014b3f7a017059e3585ae9771c3bc1a
Kapil Bhalodiya <kapilbhalodiya91041@gmail.com>
2025-06-23 10:28:32 +0530

    Update frontend Code and Add Bakcned Jenkins

--- frontend/src/app/pages/checkout/checkout.component.ts
+++ frontend/src/app/pages/checkout/checkout.component.ts
@@ -14,13 +14,11 @@ import { CartItem } from '../../models/cartItem.model';
 import { AddressService } from '../../services/address.service';
 import { Address } from '../../models/address.model';
 import { SpinnerComponent } from "../../compoments/spinner/spinner.component";
-import { HeaderComponent } from "../../compoments/header/header.component";
-
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.scss'],
-  imports: [StepperComponent, CommonModule, FormsModule, SpinnerComponent, HeaderComponent],
+  imports: [StepperComponent, CommonModule, FormsModule, SpinnerComponent],
 })
 export class CheckoutComponent implements OnInit, OnDestroy {
   @ViewChild(StepperComponent) formStepper!: StepperComponent;
