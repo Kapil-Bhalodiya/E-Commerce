b880adb6908fb297d94d7dde80836034b466d474
Kapil Bhalodiya <kapilbhalodiya91041@gmail.com>
2025-07-01 14:48:10 +0530

    update env uel for server app

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://www.sharkapp.local:8000/',
-  backendApi: 'http://www.sharkapp.local:8000/api',
+  apiUrl: 'http://dev.sharkapp.local:8000/',
+  backendApi: 'http://dev.sharkapp.local:8000/api',
   enableLogging: false,
   enableDebug: false,
   cacheTimeout: 600000, // 10 minutes
@@ -9,4 +9,4 @@ export const environment = {
 }
 
 // Use environment variable or secure config service in production
-export const STRIPE_PK = "pk_live_your_production_stripe_key"
+export const STRIPE_PK = "pk_test_51REP3lDBxXhwa9k2yzLbnYdr8eS6DuHnP9SbsYouNdlkZPK8GRhVfF1Bh9iHR1Vz5VwWc1b5BUIjKb5tJtPMT6lp00Ke5xIjSX"
